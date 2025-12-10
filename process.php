ecf4513f80a6484a0073d9d9c01c65cdc5699ad7
mnsakshi20-bit <mnsakshi20@gmail.com>
2025-12-10 20:46:12 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head><title>Registration Successful</title><link rel="stylesheet" href="css/style.css"></head>
+<body>
+<div class="container">
+<h2>Registration Successful</h2>
+<p><strong>Name:</strong> <?php echo $_POST['Sakshi M N']; ?></p>
+<p><strong>Email:</strong> <?php echo $_POST['mnsalshi20@gmail.com']; ?></p>
+<p><strong>Phone:</strong> <?php echo $_POST['8088671758']; ?></p>
+<p><strong>Address:</strong> <?php echo $_POST['Chikkamagalur']; ?></p>
+<p><strong>Course Selected:</strong> <?php echo $_POST['BE']; ?></p>
+</div>
+</body>
+</html>
